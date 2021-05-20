e4607499497a6b48830af9b05be541a32f879f24
hmorfeld <hmorfeld91@gmail.com>
2021-05-20 18:07:35 -0500

    Added Menu Items and Menu

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,4 +1,38 @@
 package menu;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Menu {
+
+    private Date lastUpdated;
+    private ArrayList<MenuItem> items;
+
+    public Menu(Date lastUpdated, ArrayList<MenuItem> items) {
+        this.lastUpdated = lastUpdated;
+        this.items = items;
+    }
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public ArrayList<MenuItem> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<MenuItem> items) {
+        this.items = items;
+    }
+
+    public ArrayList<MenuItem> addItem(MenuItem item) {
+        this.items.add(item);
+        return this.items;
+    }
+
 }
